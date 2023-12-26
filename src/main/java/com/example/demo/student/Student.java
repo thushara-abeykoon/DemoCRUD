3071dd7ccc38add36f2f866e1037f9f2ea7c55e0
Thushara Abeykoon <thushara.abeykoon99@gmail.com>
2023-12-26 15:57:12 +0530

    changed Student Entity to get age by calculation changed StudentService class to getStudents from database

--- src/main/java/com/example/demo/student/Student.java
+++ src/main/java/com/example/demo/student/Student.java
@@ -3,6 +3,7 @@ package com.example.demo.student;
 import jakarta.persistence.*;
 import org.springframework.data.annotation.Transient;
 import java.time.LocalDate;
+import java.time.Period;
 
 @Entity
 @Table
@@ -65,7 +66,7 @@ public class Student {
     }
 
     public Integer getAge() {
-        return age;
+        return Period.between(this.dob,LocalDate.now()).getYears();
     }
 
     public void setAge(Integer age) {
